d76b89f427211befe208aac370ed8c5d2aafbc43
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-10-16 17:46:00 +0200

    even test

--- test.js
+++ test.js
@@ -9,3 +9,12 @@ tape('odd-ordered moving averages', t => {
   )
   t.end()
 })
+
+tape('even-ordered moving averages (automatically ceiled and centered)', t => {
+  t.same(
+    ma([ 1, 2, 3, 4, 5 ], 2),
+    [ null, 1.3333333333333333, 2, 2.6666666666666665, null ],
+    '2-ma of seq 12345 (auto ceil + center)'
+  )
+  t.end()
+})
