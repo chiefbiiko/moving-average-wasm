311e9488868ea3cf0e9300986401937cfa38412e
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-10-22 11:11:22 +0200

    set input into typed view

--- test.wat.js
+++ test.wat.js
@@ -4,10 +4,15 @@ const wasm = require('./index.wat.js')()
 tape('f64_sum', t => { // dev test
   const arr = [ 1, 2, 3, 4, 5 ]
 
-  var f64 = new Float64Array(wasm.exports.memory.buffer)
-  for (var i = 0; i < arr.length; i++) f64[i] = arr[i]
+  new Float64Array(wasm.exports.memory.buffer).set(arr, 0, arr.length)
 
-  const sum = wasm.exports.f64_sum(0, 5 * 8)
+  const sum = wasm.exports.f64_sum(0, arr.length * 8)
   t.equal(sum, 15, 'sum is 15')
   t.end()
+})
+
+tape.skip('f64_ma - odd-ordered moving average', t => {
+  const arr = [ 1, 2, 3, 4, 5 ]
+  const exp = new Float64Array([ null, 2, 3, 4, null ])
+
 })
