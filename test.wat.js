d2d5811c6c1ce589cabec88d1cd94ed5f4c87edd
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-10-22 11:45:04 +0200

    out of bounds

--- test.wat.js
+++ test.wat.js
@@ -14,14 +14,13 @@ tape('f64_sum', t => { // dev test
 tape.only('f64_ma - odd-ordered moving average', t => {
   const in_arr = [ 1, 2, 3, 4, 5 ]
   const in_ptr = 0
+  const in_arr_byte_length = in_arr.length * 8
 
   new Float64Array(wasm.exports.memory.buffer).set(in_arr, in_ptr, in_arr.length)
-  const out_arr = new Float64Array(wasm.exports.memory.buffer, in_arr.length, in_arr.length)
+  const out_arr = new Float64Array(wasm.exports.memory.buffer, in_arr_byte_length, in_arr.length)
   // const exp = new Float64Array([ null, 2, 3, 4, null ])
 
   t.same(out_arr, new Float64Array([ 0, 0, 0, 0, 0 ]), 'same typed arr')
-
-  const in_arr_byte_length = in_arr.length * 8
 
   wasm.exports.f64_ma(
     in_ptr, // in_arr ptr into memory
